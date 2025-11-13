58d0f13e3ad82872adbb0881decbbf985e4f91a2
pedrodanielpereztorres-maker <pedrodanielpereztorres@gmail.com>
2025-11-13 11:58:08 -0400

    'change_db_string'

--- rxconfig.py
+++ rxconfig.py
@@ -3,7 +3,8 @@ import os
 
 config = rx.Config(
     app_name="pagina_web",
-    db_url=os.environ.get("DATABASE_URL", "postgresql://postgres:V32273930@localhost:5432/mi_pagina"),
+    db_url=os.environ.get(
+        "DATABASE_URL", "postgresql://mi_pagina_user:1QXSGYeVql8XEH0jl9ZmFPjM2bCoAVgZ@dpg-d49lmichg0os738t280g-a/mi_pagina"),
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
